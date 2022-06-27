5c8a3381c751c9e8366a82fe3c0f7d7ef5dd30cf
dimavolkov1704 <dima.volkov1704@gmail.com>
2022-06-27 10:43:30 +0300

    Volkov Dima IS-01

---
+++ StructuralPatterns/EncryptionFacade.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace StructuralPatterns
+{
+ 
+    static public class EncryptionFacade
+    {
+        static public byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            byte[] encrypted;
+
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            return encrypted;
+        }
+
+        static public string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            string plaintext = null;
+
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
+    }
+}
---
+++ StructuralPatterns/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace StructuralPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "Here is some data to encrypt!";
+
+            using (AesManaged myAes = new AesManaged())
+            {
+
+                byte[] encrypted = EncryptionFacade.EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+                string roundtrip = EncryptionFacade.DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+                Print(original, encrypted, roundtrip);
+       
+            }
+        }
+
+        static public void Print(string original, byte[] encrypted, string roundtrip)
+        {
+            Console.WriteLine("Original:   {0}", original);
+            Console.WriteLine("encrypted:");
+            for (int i = 0; i < encrypted.Length; i++)
+            {
+                Console.Write(encrypted[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Round Trip: {0}", roundtrip);
+        }
+    }
+}
+
+
+
+
